f72beb476c4575c47e1d73f49ea72ecd85975644
Francesco Sblendorio <sblendorio@gmail.com>
2018-05-27 14:58:56 +0200

    Release 0.2: automatic resize before printing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cbmage 0.1
+# cbmage 0.2
 Print a picture from a modern PC with an old-fashioned **Commodore MPS 803** compatible printer
 ## Purpose
 This utility outputs raw bytes for printing on a [Commodore MPS 803](http://www.zimmers.net/cbmpics/p6serial3.html) compatible printer. It should be used with [**opencbm**](http://spiro.trikaliotis.net/opencbm) package by [Spiro Trikaliotis](http://spiro.trikaliotis.net/) to redirect the output to the printer, connected through a [XUM1541 cable](https://rdist.root.org/2009/01/21/introducing-xum1541-the-fast-c64-floppy-usb-adapter/), such as [ZoomFloppy](http://www.go4retro.com/products/zoomfloppy/).
@@ -7,6 +7,7 @@ This utility outputs raw bytes for printing on a [Commodore MPS 803](http://www.
 - **source** - Source code in C
   - [**cbmage.c**](https://github.com/sblendorio/cbmage/blob/master/source/cbmage.c) - Main program
   - [**stb_image.h**](https://github.com/sblendorio/cbmage/blob/master/source/stb_image.h) - [STB library](https://github.com/nothings/stb) by [Sean Barrett](https://twitter.com/nothings) for reading image files
+  - [**stb_image_resize.h**](https://github.com/sblendorio/cbmage/blob/master/source/stb_image_resize.h) - [STB library](https://github.com/nothings/stb) by [Sean Barrett](https://twitter.com/nothings) for resizing images
   - [**Makefile**](https://github.com/sblendorio/cbmage/blob/master/source/Makefile) - Makefile
 - **binaries** - Precompiled executable files for different platforms
   - **win32**/[**cbmage.exe**](https://github.com/sblendorio/cbmage/blob/master/binaries/win32/cbmage.exe?raw=true) - for **Windows XP**
@@ -59,7 +60,8 @@ The typical sequence of command you should use to do the task is:
 In particular, the **4th line** (`./cbmage picture.png | cbmctrl write`) produces the raw bytes (launch it **without "./"** if you run it **on Windows**), which are redirected to the printer through the piped `cbmctrl write` command.
 
 ## Restrictions
-**Maximum width** of the picture is **480 pixel**, the printer is a **black and white** one. The _recommended_ format is **PNG**: every single dot which is **white** (total white: `#ffffff` in hex) will be left blank, while **any** other pixel color will result in a **black** dot on the printer.
+* **Maximum width** on a Commodore MPS 803 is **480 dots** per row, so the image will be automatically resized to that width if it exceeds that size.
+* The printer is a **black and white** one. The _recommended_ format is **PNG**: every single dot which is **white** (total white: `#ffffff` in hex) will be left blank, while **any** other pixel color will result in a **black** dot on the printer.
 
 ## Credits
 Thanks to [**Spiro Trikaliotis**](http://spiro.trikaliotis.net/) for the [opencbm package](http://spiro.trikaliotis.net/opencbm), to [**Till Harbaum**](http://spiro.trikaliotis.net/xu1541) for the initial case study of the [XU1541](http://spiro.trikaliotis.net/xu1541) (and also for the fantastic [MIST](http://harbaum.org/till/mist/index.shtml)) and to [**Sean Barrett**](https://twitter.com/nothings) for his powerful and effective [STB Image Library](https://github.com/nothings/stb).
--- source/cbmage.c
+++ source/cbmage.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-void print(unsigned char *data, int width, int height);
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
+#define MAXWIDTH 480
+
+void print(FILE *out, unsigned char *data, int width, int height);
 int  getPixel(unsigned char *data, int width, int height, int x, int y, int bpp);
 void usage(int argc, char *argv[]);
 void die(char *message);
+unsigned char *resize_width(unsigned char *input, int *in_w, int *in_h, int depth, int out_w);
 
 int main(int argc, char *argv[]) {
 	int width, height, bpp;
-	unsigned char *data;
+	unsigned char *data, *resized;
 
 	usage(argc, argv);
 	data = stbi_load(argv[1], &width, &height, &bpp, 3);
 	if (data == NULL) die("I/O error: file not found");
 
-	print(data, width, height);
+	if (width > MAXWIDTH) {
+		resized = resize_width(data, &width, &height, 3, MAXWIDTH);
+		fprintf(stderr,
+			"Image exceeds %d dots per row: resizing to %dx%d before printing.\n", 
+			MAXWIDTH, width, height);
+		data = resized;
+	}
+
+	print(stdout, data, width, height);
 	stbi_image_free(data);
 	return 0;
 }
 
-void print(unsigned char *data, int width, int height) {
+void print(FILE *out, unsigned char *data, int width, int height) {
 	static int pow[] = {1, 2, 4, 8, 16, 32, 64};
 	int row, x, dy, rows, code, pixel;
 	
 	rows = height / 7 + (height % 7 == 0 ? 0 : 1);
-	printf("%c", 8);
+	fprintf(out, "%c", 8);
 	for (row=0; row<rows; ++row) {
 		for (x=0; x<width; ++x) {
 			code = 128;
@@ -33,11 +48,11 @@ void print(unsigned char *data, int width, int height) {
 				pixel = getPixel(data, 3, width, height, x, (row*7) + dy);
 				code += pow[dy]*pixel;
 			}
-			printf("%c", code);
+			fprintf(out, "%c", code);
 		}
-		printf("%c", 13);
+		fprintf(out, "%c", 13);
 	}
-	printf("%c%c", 15, 13);
+	fprintf(out, "%c%c", 15, 13);
 }
 
 int getPixel(unsigned char *data, int bpp, int width, int height, int x, int y) {
@@ -53,22 +68,35 @@ int getPixel(unsigned char *data, int bpp, int width, int height, int x, int y) 
 
 void usage(int argc, char *argv[]) {
 	if (argc > 1) return;
-	printf("cbmage 0.1\n\n");
-	printf("Usage: %s <image file name>\n\n", argc == 0 ? "cbmage" : argv[0]);
-	printf("Outputs raw bytes for printing on a CBM MPS 803 compatible printer.\n");
-	printf("It should be used with opencbm package by Spiro Trikaliotis to redirect the\n");
-	printf("output to the printer, connected through a XUM1541 cable or Zoom Floppy.\n");
-	printf("Max width of the picture: 480 pixels. It\'s not automatically resized.\n");
-	printf("\n");
-	printf("Example:\n\n");
-	printf("cbmctrl reset\ncbmctrl lock\ncbmctrl listen 4 0\n");
-	printf("%s picture.png | cbmwrite\n", argc == 0 ? "cbmage" : argv[0]);
-	printf("cbmctrl unlisten\ncbmctrl unlock\n");
-	printf("\n");
+	fprintf(stderr, "cbmage 0.2\n\n");
+	fprintf(stderr, "Usage: %s <image file name>\n\n", argc == 0 ? "cbmage" : argv[0]);
+	fprintf(stderr, "Outputs raw bytes for printing on a CBM MPS 803 compatible printer.\n");
+	fprintf(stderr, "It should be used with opencbm package by Spiro Trikaliotis to redirect the\n");
+	fprintf(stderr, "output to the printer, connected through a XUM1541 cable or Zoom Floppy.\n");
+	fprintf(stderr, "Max picture width: 480 pixels. It will be automatically resized if exceeds.\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "Example:\n\n");
+	fprintf(stderr, "cbmctrl reset\ncbmctrl lock\ncbmctrl listen 4 0\n");
+	fprintf(stderr, "%s picture.png | cbmwrite\n", argc == 0 ? "cbmage" : argv[0]);
+	fprintf(stderr, "cbmctrl unlisten\ncbmctrl unlock\n");
+	fprintf(stderr, "\n");
 	exit(1);
 }
 
 void die(char *message){
 	fprintf(stderr, "%s\n", message);
 	exit(2);
+}
+
+unsigned char *resize_width(unsigned char *input, int *in_w, int *in_h, int depth, int out_w) {
+	int out_h;
+	unsigned char *output;
+	out_h = (*in_h * out_w / *in_w) + ((*in_h * out_w % *in_w) == 0 ? 0 : 1);
+	
+	output = (unsigned char*) malloc(out_w * out_h * depth);
+	stbir_resize_uint8(input, *in_w, *in_h, 0, output, out_w, out_h, 0, depth);
+	stbi_image_free(input);
+	*in_w = out_w;
+	*in_h = out_h;
+	return output;
 }
